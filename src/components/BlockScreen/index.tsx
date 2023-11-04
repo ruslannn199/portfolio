7f02af8aee2d096f844029239025caea056c714f
ruslannn199 <ruslannn199@gmail.com>
2023-11-04 22:08:07 +0300

    fix(components): edit bug with burger popup

--- src/components/BlockScreen/index.tsx
+++ src/components/BlockScreen/index.tsx
@@ -1,11 +1,14 @@
+import useWindowDimensions from '../../hooks';
+
 interface BlockScreenProps {
   blocked: boolean;
   unblock: () => void;
 }
 
 const BlockScreen: React.FC<BlockScreenProps> = ({ blocked, unblock }) => {
+  const { width } = useWindowDimensions();
   return <div className="block-screen" style={
-    blocked
+    width < 992 && blocked
       ? { width: '100%', opacity: 1 }
       : { width: 0, opacity: 0 }
   } onClick={unblock} />;
