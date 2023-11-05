32a9230f155846fc9f10abbba9344f336bef5337
ruslannn199 <ruslannn199@gmail.com>
2023-11-05 15:18:07 +0300

    feat(components): style portfolio section

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -1,18 +1,58 @@
 import PortfolioCard, { PortfolioCardValues } from '../PortfolioCard';
 import Wrapper from '../Wrapper';
+import BlankImg from '../../../public/images/projects/blank.png';
+import RSCSS from '../../../public/images/projects/rs-css.png';
+import ReactMovie from '../../../public/images/projects/react-movie.png';
+import CozyHouse from '../../../public/images/projects/cozy-house.png';
 
 const Portfolio: React.FC = () => {
-  const PortfolioCards: PortfolioCardValues[] = [];
+  const PortfolioCards: PortfolioCardValues[] = [
+    {
+      title: 'Fundle-games 🎲',
+      img: BlankImg,
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, ipsa! Voluptate laborum ut adipisci placeat, ex reprehenderit non, odit unde nihil dolorem earum, nobis doloribus vitae dolorum aperiam explicabo quas!',
+      techs: ['React', 'Redux', 'Redux Saga', 'Styled-components', 'AntD', 'Typescript', 'React Router'],
+      code: 'https://github.com/ruslannn199/fundle-games',
+      deploy: 'https://fundle-games.infinityfreeapp.com',
+    },
+    {
+      title: 'CSS Dinner 🍽️',
+      img: RSCSS,
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, ipsa! Voluptate laborum ut adipisci placeat, ex reprehenderit non, odit unde nihil dolorem earum, nobis doloribus vitae dolorum aperiam explicabo quas!',
+      techs: ['Typescript', 'Sass', 'Jest'],
+      code: 'https://github.com/ruslannn199/',
+      deploy: 'https://rolling-scopes-school.github.io/ruslannn199-JSFE2023Q1/rs-css/',
+    },
+    {
+      title: 'React Movie 🎥',
+      img: ReactMovie,
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, ipsa! Voluptate laborum ut adipisci placeat, ex reprehenderit non, odit unde nihil dolorem earum, nobis doloribus vitae dolorum aperiam explicabo quas!',
+      techs: ['React', 'Typescript', 'Styled-components', 'React Router'],
+      code: 'https://github.com/ruslannn199/react-movie',
+      deploy: 'https://react-movie-ruslan-kamenskiy.netlify.app/',
+    },
+    {
+      title: 'Cozy House 🦴',
+      img: CozyHouse,
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, ipsa! Voluptate laborum ut adipisci placeat, ex reprehenderit non, odit unde nihil dolorem earum, nobis doloribus vitae dolorum aperiam explicabo quas!',
+      techs: ['Typescript', 'Sass'],
+      code: 'https://github.com/ruslannn199/cozy-house',
+      deploy: 'https://ruslannn199.github.io/cozy-house/public/main/',
+    },
+  ];
+
   return (
-    <section id="portfolio" className="portfolio section">
+    <main id="portfolio" className="portfolio">
       <Wrapper>
         <div className="portfolio__content">
-          <p>portfolio</p>
-          <h3>Each project is a unique piece of development 🧩</h3>
-          {...(PortfolioCards.map((card) => (<PortfolioCard card={card} />)))}
+          <h2 className="portfolio__title">portfolio</h2>
+          <h3 className="portfolio__subtitle">Each project is a unique piece of development 🧩</h3>
+          <section className="portfolio__cards">
+            {...(PortfolioCards.map((card, i) => (<PortfolioCard key={i} card={card} reversed={(i % 2 !== 0)} />)))}
+          </section>
         </div>
       </Wrapper>
-    </section>
+    </main>
   );
 }
 
--- src/components/PortfolioCard/index.tsx
+++ src/components/PortfolioCard/index.tsx
@@ -11,27 +11,29 @@ export interface PortfolioCardValues {
 
 interface PortfolioCardProps {
   card: PortfolioCardValues;
+  reversed: boolean;
 }
 
-const PortfolioCard: React.FC<PortfolioCardProps> = ({ card: {
-  title, img, desc, techs, code, deploy
-}}) => {
+const PortfolioCard: React.FC<PortfolioCardProps> = ({
+  card: { title, img, desc, techs, code, deploy }, reversed
+}) => {
+  console.log(reversed);
   return (
-    <div className="portfolio__card">
-      <a target="_blank" href={deploy} rel="noreferrer">
-        <img src={img} alt={title} />
+    <div className={`portfolio__card${reversed ? ' portfolio__card_reversed' : ''}`}>
+      <a className="portfolio__card-link" target="_blank" href={deploy} rel="noreferrer">
+        <img className="portfolio__card-img" src={img} alt={title} />
       </a>
       <article className="portfolio__details">
-        <h3>{title}</h3>
-        <p>{desc}</p>
+        <h3 className="portfolio__details-title">{title}</h3>
+        <h4 className="portfolio__details-subtitle">{desc}</h4>
         <div className="portfolio__stack">
-          {...(techs.map((tech) => (<p>{tech}</p>)))}
+          {...(techs.map((tech, key) => (<p key={key} className="portfolio__stack-item">{tech}</p>)))}
         </div>
         <div className="portfolio__links">
-          <a target="_blank" href={code} rel="noreferrer">
+          <a className="portfolio__link" target="_blank" href={code} rel="noreferrer">
             Code <IconBrandGithub />
           </a>
-          <a target="_blank" href={deploy} rel="noreferrer">
+          <a className="portfolio__link" target="_blank" href={deploy} rel="noreferrer">
             Live Demo <IconExternalLink />
           </a>
         </div>
