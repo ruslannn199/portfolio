e0f6493aa9182d86df76d431a8b87f87bd4ee234
ruslannn199 <ruslannn199@gmail.com>
2023-11-18 22:30:06 +0300

    fix: fix minor issues

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -5,7 +5,7 @@ import Wrapper from '../Wrapper';
 
 const About: React.FC = () => {
   return (
-    <section id="about" className="about">
+    <section className="about">
       <Wrapper>
         <div className="about__content">
           <div className="about__images">
@@ -16,9 +16,9 @@ const About: React.FC = () => {
             </span>
           </div>
           <article className="about__article">
-            <h3 className="about__title">About me</h3>
+            <h3 id="about" className="about__title">About me</h3>
             <h4 className="about__subtitle">
-              A dedicated Front-end Developer <br /> based in Saint Petersburg, Russia 📍
+              A dedicated Front-end Developer <br /> based in Saint&nbsp;Petersburg,&nbsp;Russia&nbsp;📍
             </h4>
             <p className="about__text">
               As a Front-End Developer, I possess an impressive arsenal
@@ -26,10 +26,11 @@ const About: React.FC = () => {
               I excel in designing and maintaining responsive websites that
               offer a smooth user experience. My expertise lies in crafting
               dynamic, engaging interfaces through writing clean and optimized
-              code and utilizing cutting-edge development tools and
-              techniques. I am also a team player who thrives in collaborating
-              with cross-functional teams to produce outstanding web
-              applications.
+              code and utilizing cutting-edge development tools and techniques.
+              My ability to adapt to different brand voices and styles
+              allows me to deliver impactful messaging across various platforms.
+              My proactive approach and communication skills
+              ensure seamless collaboration and efficient project execution.
             </p>
           </article>
         </div>
